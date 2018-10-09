590b19cacf815c1cc1178436dc1899251dc72aa3
Phong Hoai Vo <vo.hoaiphongamkg@gmail.com>
2018-10-09 20:11:06 +0700

    Revert "Merge code pond"
    This reverts commit b4d6c5450490ab0cb6eaa7337a455d5055031017.

--- src/app/contants/http.ts
+++ src/app/contants/http.ts
@@ -1,10 +1,11 @@
 import { HttpHeaders } from "@angular/common/http";
 
-export const APP_JSON = new HttpHeaders({
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
-})
-
-export const APP_FORM_FILE = new HttpHeaders({
-    'Content-Type': 'multipart/form-data'
-})
+export const headers: any = {
+    APP_JSON: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+    }),
+    APP_FORM_FILE: new HttpHeaders({
+        'Content-Type': 'multipart/form-data'
+    })
+}
