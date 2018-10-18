b6f060995189736343b7b66be9d6eb87e63ad508
Phong Hoai Vo <vo.hoaiphongamkg@gmail.com>
2018-10-18 15:17:24 +0700

    edit pond-detail

--- src/app/pond-management/detail-pond/detail-pond.component.ts
+++ src/app/pond-management/detail-pond/detail-pond.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material';
 import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { MY_FORMATS_DATE } from '../../constants/format-date';
 import { PondManagementService } from '../pond-management.service';
+import { AppService } from 'src/app/app.service';
+import { tokenName } from '../../../environments';
 
 interface marker {
   lat: number;
@@ -23,21 +25,32 @@ interface marker {
   ],
 })
 export class DetailPondComponent implements OnInit {
-
+  imageLink: string = '';
+  private errorFile: Promise<string> | null = null;
+  preloader: boolean = false;
+  timeOut: boolean = false;
+  imgSource: string;
   public form: FormGroup;
   constructor(
     private fb: FormBuilder,
     private adapter: DateAdapter<any>,
-    private pondManagementService: PondManagementService
+    private pondManagementService: PondManagementService,
+    private cd: ChangeDetectorRef,
+    private appService: AppService
   ) { }
 
   ngOnInit() {
     this.form = this.fb.group({
-      pond: [null, Validators.compose([Validators.required])],
-      pond_date: [null, Validators.compose([Validators.required])],
-      pondarea: [null, Validators.compose([Validators.required])],
-      ponddepth: [null, Validators.compose([Validators.required])],
-      pondstatus: [null, Validators.compose([Validators.required])]
+      pondName: [null, Validators.compose([Validators.required])],
+      pondCreatedDate: [null, Validators.compose([Validators.required])],
+      pondArea: [null, Validators.compose([Validators.required])],
+      pondDepth: [null, Validators.compose([Validators.required])],
+      pondStatus: [null, Validators.compose([Validators.required])],
+      createCost: [null, Validators.compose([Validators.required])],
+      pondLatitude: [null, Validators.compose([])],
+      pondLongitude: [null, Validators.compose([])],
+      images: [null, Validators.compose([])],
+      imageDisable: [null, Validators.compose([])],
     });
     this.form.disable();
   }
@@ -81,6 +94,51 @@ export class DetailPondComponent implements OnInit {
   changeEdit() {
     this.form.enable();
     this.editName();
+  }
+
+  checkFile(fileType: string): boolean {
+    if (fileType.split('/')[0] === 'image') {
+      return true;
+    }
+    return false;
+  }
+
+  getImage(): any {
+    let styles = {
+      'background-image': `url("${this.imageLink || "https://via.placeholder.com/360x360"}")`,
+      'background-repeat': `no-repeat`,
+      'background-size': `cover`,
+      'background-position': 'center'
+    };
+    return styles;
+  }
+
+  onFileChange(event) {
+    this.preloader = true;
+    const token: string = this.appService.getCookie(tokenName);
+    if (event.target.files && event.target.files.length) {
+      const [files]: File[] = event.target.files;
+      this.cd.markForCheck();
+      if (this.checkFile(files.type)) {
+        this.pondManagementService.uploadImage(files, token).subscribe((res: any) => {
+          this.pondManagementService.loadImage(res.fileId).subscribe(data => {
+            if (data) {
+              this.imageLink = (data as any).data;
+              this.preloader = !this.preloader;
+            }
+          });
+          this.imgSource = res.fileId;
+          console.log(res);
+        })
+      } else {
+        this.timeOut = !this.timeOut;
+        this.errorFile = Promise.resolve("Hình ảnh không được cho phép, vui lòng thử lại!")
+        setTimeout(() => {
+          this.timeOut = !this.timeOut;
+        }, 5000);
+        this.preloader = !this.preloader;
+      }
+    }
   }
 
 }
