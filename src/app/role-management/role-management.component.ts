20227173453a43b95820a53e1fa90a880ae5a2e1
Nguyen Viet Huan <ngnviethuan@gmail.com>
2018-11-09 04:27:58 +0700

    :rose: fix change roles

--- src/app/role-management/role-management.component.ts
+++ src/app/role-management/role-management.component.ts
@@ -11,171 +11,187 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DialogAddRoleEmpManagement } from './dialog-add-role-emp-management.component';
 
 @Component({
-  selector: 'app-role-management',
-  templateUrl: './role-management.component.html',
-  styleUrls: ['./role-management.component.scss']
+	selector: 'app-role-management',
+	templateUrl: './role-management.component.html',
+	styleUrls: ['./role-management.component.scss']
 })
 export class RoleManagementComponent implements OnInit {
-  ELEMENT_DATA: Users[]
-  displayedColumns: string[] = ['name', 'roles', 'action'];
-  dataSource = new MatTableDataSource<Users>(this.ELEMENT_DATA);
-  token: any;
+	ELEMENT_DATA: Users[]
+	displayedColumns: string[] = ['name', 'roles', 'action'];
+	dataSource = new MatTableDataSource<Users>(this.ELEMENT_DATA);
+	token: any;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
-  animal: string;
-  name: string;
-  constructor(
-    private router: Router,
-    private employeesManagementService: EmployeesManagementService,
-    public snackBar: MatSnackBar,
-    private appService: AppService,
-    public dialog: MatDialog
-  ) {}
+	animal: string;
+	name: string;
+	constructor(
+		private router: Router,
+		private employeesManagementService: EmployeesManagementService,
+		public snackBar: MatSnackBar,
+		private appService: AppService,
+		public dialog: MatDialog
+	) { }
 
-  ngOnInit() {
-    this.token = this.appService.getCookie(tokenName);
-    this.updateTable();
-  }
+	ngOnInit() {
+		this.token = this.appService.getCookie(tokenName);
+		this.updateTable();
+	}
 
-  openDialogAddRoleManagement(rolesId): void {
-    const dialogRef = this.dialog.open(DialogAddRoleManagement, {
-      width: '280px',
-      data: {rolesId}
-    });
+	openDialogAddRoleManagement(rolesId): void {
+		const dialogRef = this.dialog.open(DialogAddRoleManagement, {
+			width: '280px',
+			data: { rolesId }
+		});
 
-    dialogRef.afterClosed().subscribe(result => {
-      this.updateTable()
-    });
-  }
+		dialogRef.afterClosed().subscribe(result => {
+			this.updateTable()
+		});
+	}
 
-  updateTable(){
-    this.employeesManagementService.getEmployee(this.token).subscribe((res: any) => {
-      const arrayResult: Users[] = [];
-      for(let element of (res.employees as Users[])){
-        for(let role of element.roles){
-          arrayResult.push({
-            userId: element.userId,
-            firstname: element.firstname,
-            lastname: element.lastname,
-            role
-          })
-        }
-      }
-      this.ELEMENT_DATA = arrayResult;
-      this.dataSource = new MatTableDataSource<Users>(this.ELEMENT_DATA);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    })
-  }
+	updateTable() {
+		this.employeesManagementService.getEmployee(this.token).subscribe((res: any) => {
+			const arrayResult: Users[] = [];
+			for (let element of (res.employees as Users[])) {
+				for (let role of element.roles) {
+					arrayResult.push({
+						userId: element.userId,
+						firstname: element.firstname,
+						lastname: element.lastname,
+						role
+					})
+				}
+			}
+			this.ELEMENT_DATA = arrayResult;
+			this.dataSource = new MatTableDataSource<Users>(this.ELEMENT_DATA);
+			this.dataSource.paginator = this.paginator;
+			this.dataSource.sort = this.sort;
+		})
+	}
 
-  openDialogAddRoleEmpManagement(): void {
-    const dialogRef = this.dialog.open(DialogAddRoleEmpManagement, {
-      width: '280px',
-      // data: {rolesId}
-    });
+	openDialogAddRoleEmpManagement(): void {
+		const dialogRef = this.dialog.open(DialogAddRoleEmpManagement, {
+			width: '280px',
+			// data: {rolesId}
+		});
 
-    dialogRef.afterClosed().subscribe(result => {
-      this.updateTable()
-    });
-  }
+		dialogRef.afterClosed().subscribe(result => {
+			this.updateTable()
+		});
+	}
 
-  deletedRole(userId,roles) {
-    this.employeesManagementService.upsertUserRole({
-      userId,
-      roles,
-      isDeleted: 1
-    }, this.token).subscribe(res => {
-      if(res.success) {
-        this.snackBar.open('Đã xóa thành công.', 'Đóng', {
-          duration: 2500,
-          horizontalPosition: "right",
-          verticalPosition: 'bottom'
-        });
-        this.updateTable()
-      } else {
-        this.snackBar.open(res.message, 'Đóng', {
-          duration: 2500,
-          horizontalPosition: "right",
-          verticalPosition: 'bottom'
-        });
-      }
-    })
-  }
+	deletedRole(userId, roles) {
+		this.employeesManagementService.upsertUserRole({
+			userId,
+			roles,
+			isDeleted: 1
+		}, this.token).subscribe(res => {
+			if (res.success) {
+				this.snackBar.open('Đã xóa thành công.', 'Đóng', {
+					duration: 2500,
+					horizontalPosition: "right",
+					verticalPosition: 'bottom'
+				});
+				this.updateTable()
+			} else {
+				this.snackBar.open(res.message, 'Đóng', {
+					duration: 2500,
+					horizontalPosition: "right",
+					verticalPosition: 'bottom'
+				});
+			}
+		})
+	}
 }
 
 
 
 @Component({
-  selector: 'dialog-add-role-management',
-  templateUrl: './dialog-add-role-management.html',
+	selector: 'dialog-add-role-management',
+	templateUrl: './dialog-add-role-management.html',
 })
 export class DialogAddRoleManagement implements OnInit {
-  token: any;
-  name: string;
-  public form: FormGroup;
-  rolesList: any[] = [
-    {
-      value: 1,
-      label: 'Quản lý ao nuôi'
-    },
-    {
-      value: 2,
-      label: 'Quản lý kho'
-    }
-  ];
-  constructor(
-    public dialogRef: MatDialogRef<DialogAddRoleManagement>,
-    private appService: AppService,
-    private fb: FormBuilder,
-    public snackBar: MatSnackBar,
-    private employeesManagementService: EmployeesManagementService,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
-  ) {
-    this.token = this.appService.getCookie(tokenName);
-  }
+	token: any;
+	role: any = {
+		users: {
+			firstname: '',
+			lastname: ''
+		},
+	};
+	public form: FormGroup;
+	rolesList: any[] = [
+		{
+			value: 1,
+			label: 'Quản lý ao nuôi'
+		},
+		{
+			value: 2,
+			label: 'Quản lý kho'
+		}
+	];
+	constructor(
+		public dialogRef: MatDialogRef<DialogAddRoleManagement>,
+		private appService: AppService,
+		private fb: FormBuilder,
+		public snackBar: MatSnackBar,
+		private employeesManagementService: EmployeesManagementService,
+		@Inject(MAT_DIALOG_DATA) public data: DialogData
+	) {
+		this.token = this.appService.getCookie(tokenName);
+	}
 
-    ngOnInit() {
-      this.createForm();
-      this.employeesManagementService.getEmployeeById(this.token, (this.data as any).rolesId).subscribe((res: any) => {
-        this.name = res.roles.users.lastname + ' ' + res.roles.users.firstname;
-        this.form.patchValue({
-          roles: res.roles.roles
-        })
-      })
-    }
+	ngOnInit() {
+		this.createForm();
+		this.loadData();
+	}
 
-    createForm() {
-      this.form = this.fb.group({
-        roles: [null, Validators.compose([Validators.required])]
-      })
-    }
+	loadData = () => {
+		this.employeesManagementService.getEmployeeById(this.token, (this.data as any).rolesId).subscribe((res: any) => {
+			if (res.success) {
+				this.role = res.roles;
+			} else {
+				this.snackBar.open(res.message, 'Đóng', {
+					duration: 2500,
+					horizontalPosition: "right",
+					verticalPosition: 'bottom'
+				});
+			}
+		})
+	}
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+	createForm() {
+		this.form = this.fb.group({
+			roles: [null, Validators.compose([Validators.required])]
+		})
+	}
 
-  onSubmit() {
-    this.employeesManagementService.updateRolesEmployee(this.token,{
-      rolesId: (this.data as any).rolesId,
-      roles: this.form.value.roles
-    }).subscribe((res: any) => {
-      if(res.success) {
-        this.dialogRef.close();
-        this.snackBar.open(res.message, 'Đóng', {
-          duration: 2500,
-          horizontalPosition: "right",
-          verticalPosition: 'bottom'
-        });
-      } else {
-        this.snackBar.open(res.message, 'Đóng', {
-          duration: 2500,
-          horizontalPosition: "right",
-          verticalPosition: 'bottom'
-        });
-      }
-    })
-  }
+	onNoClick(): void {
+		this.dialogRef.close();
+	}
+
+	onSubmit() {
+		this.employeesManagementService.changeRoles({
+			rolesId: this.role.rolesId,
+			userId: this.role.userId,
+			roles: this.form.value.roles,
+			isDeleted: 0
+		}, this.token).subscribe((res: any) => {
+			if (res.success) {
+				this.dialogRef.close();
+				this.snackBar.open(res.message, 'Đóng', {
+					duration: 2500,
+					horizontalPosition: "right",
+					verticalPosition: 'bottom'
+				});
+			} else {
+				this.snackBar.open(res.message, 'Đóng', {
+					duration: 2500,
+					horizontalPosition: "center",
+					verticalPosition: 'top'
+				});
+			}
+		})
+	}
 
 }
